c1c02fb4058e9787a8e89ba739088b96b3d216c3
-ly <91619541+heltonchristian@users.noreply.github.com>
2023-12-27 19:47:51 -0300

    Update i.sh

--- i.sh
+++ i.sh
@@ -3,9 +3,9 @@
 # Instalar programas
 sudo pacman -S --noconfirm git go bspwm sxhkd zsh kitty vim rofi nemo ranger w3m btop polybar papirus-icon-theme ttf-nerd-fonts-symbols ttf-fira-code feh redshift neofetch ncspot xclip maim pulsemixer firefox chromium picom
 
-# Mover pastas para ~/.config
+# Mover .config
 mkdir -p ~/.config
-mv -f bspwm sxhkd rofi neofetch polybar picom btop gtk-3.0 ~/.config
+mv -f .config ~/
 
 # Mover arquivos .zshrc,.xinitrc, scripts ...  para ~/
 mv -f .zshrc .xinitrc .zprofile wallpapers scripts .gtkrc-2.0 .ticker.yaml ~/
