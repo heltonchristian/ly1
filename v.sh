5450d3d32bdfd76ed5f9991f9560b58b3b1521f6
-ly <91619541+heltonchristian@users.noreply.github.com>
2024-01-01 01:49:42 -0300

    Update v.sh

--- v.sh
+++ v.sh
@@ -1,11 +1,14 @@
 sudo xbps-install -y void-repo-nonfree void-repo-multilib void-repo-nonfree-multilib && sudo xbps-install -Syyu
 sleep 5
-sudo xbps-install -y bspwm sxhkd xorg-minimal xinit zsh kitty vim xclip maim picom pipewire pipewire-pulseaudio pulsemixer rofi redshift htop nerd-fonts gtk-theme-united-gnome papirus-icon-theme papirus-folders feh neofetch firefox chromium virt-manager qemu libgcc-32bit libstdc++-32bit libdrm-32bit libglvnd-32bit nvidia-libs-32bit mono xautolock setxkbmap
+sudo xbps-install -y bspwm sxhkd xorg-minimal xinit zsh kitty vim xclip maim picom pipewire pipewire-pulseaudio pulsemixer rofi redshift htop nerd-fonts gtk-theme-united-gnome papirus-icon-theme papirus-folders feh neofetch firefox chromium virt-manager qemu nvidia-dkms libgcc-32bit libstdc++-32bit libdrm-32bit libglvnd-32bit nvidia-libs-32bit mono xautolock setxkbmap
+
+sleep 5
 
 mkdir -p ~/.config
 mv -f .config ~/
-
 mv -f .zshrc .xinitrc .zprofile wallpapers scripts ~/
+
+sleep 5
 
 chmod -R 755 ~/.config/bspwm/bspwmrc
 chmod -R 644 ~/.config/sxhkd/sxhkdrc
@@ -13,6 +16,8 @@ chmod -R 755 ~/.config/polybar/launch.sh
 chmod -R 755 ~/scripts/changewp.sh
 chmod -R 755 ~/scripts/changeKL.sh
 sudo usermod -aG wheel ly
+
+sleep 5
 
 chsh -s /bin/zsh
 
