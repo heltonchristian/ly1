6610cfe34e9d39913c57010e95409f61402ca868
-ly <91619541+heltonchristian@users.noreply.github.com>
2024-12-31 15:17:00 -0300

    Update i.sh

--- i.sh
+++ i.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Instalar programas
-sudo pacman -S --noconfirm git go bspwm sxhkd zsh kitty vim rofi nemo htop polybar papirus-icon-theme ttf-nerd-fonts-symbols ttf-fira-code ttf-font-awesome feh redshift neofetch ncspot xclip maim easyeffects firefox chromium libreoffice-fresh
+sudo pacman -S --noconfirm git go bspwm sxhkd zsh kitty vim rofi nemo htop polybar papirus-icon-theme ttf-nerd-fonts-symbols ttf-fira-code ttf-font-awesome feh redshift neofetch ncspot xclip maim easyeffects firefox chromium libreoffice-fresh orchis-theme
 
 # Mover .config
 rm -r ~/.config
@@ -15,7 +15,6 @@ chmod -R 755 ~/.config/bspwm/bspwmrc
 chmod -R 644 ~/.config/sxhkd/sxhkdrc
 chmod -R 755 ~/.config/polybar/launch.sh
 chmod -R 755 ~/scripts/changewp.sh
-chmod -R 755 ~/scripts/changeKL.sh
 
 #mouse
 echo -e 'Section "InputClass"\n     Identifier "My Mouse"\n     MatchIsPointer "yes"\n     Option "AccelerationProfile" "-1"\n     Option "AccelerationScheme" "none"\n     Option "AccelSpeed" "-1"\nEndSection' | sudo tee /etc/X11/xorg.conf.d/50-mouse-acceleration.conf
@@ -31,7 +30,7 @@ makepkg -si
 cd
 
 #AUR programas
-yay -S --noconfirm papirus-folders equilux-theme ticker
+yay -S --noconfirm papirus-folders ticker
 
 #icones, temas e fontes
 papirus-folders -C white --theme Papirus
